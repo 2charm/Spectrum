cf08804abe10257023faaea075ab22f37d205f44
kevinhuynh <khuynh23@uw.edu>
2019-06-04 01:02:34 -0700

    Reorganize file structure and complete main/about pages

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,12 +1,49 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import spectrum from "../img/2-squares.png"
+import Moment from 'react-moment';
 
+const SESSION_TOKEN = "sessionID";
 export class NewsItem extends Component {
+
+    updateCategory() {
+      if(sessionStorage.getItem(SESSION_TOKEN) !== null) {
+        let url = "https://api.spectrumnews.me/v1/news";
+        let bearer = sessionStorage.getItem(SESSION_TOKEN);
+
+      fetch(url, { 
+          method: "POST", 
+          headers: {
+            "Content-Type": "text/plain",
+            "Authorization":bearer
+          },
+          body: this.props.category
+        }
+      )
+      .then((response) => {
+        console.log(response.status)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+      }
+    }
+
     render() {
       let imgURL = this.props.urlToImage;
       let width = 8;
       let title = this.props.title;
-      title = title.substring(0, title.lastIndexOf(" - "));
+      let source = ""
+      if(title.lastIndexOf(" - ") !== -1) {
+        title = title.substring(0, title.lastIndexOf(" - "));
+      }
 
+      if(this.props.isFull) {
+        source = this.props.source.name;
+        if(source.lastIndexOf(".") !== -1) {
+          source = source.substring(0, source.lastIndexOf("."));
+        }
+      }
       if(imgURL === "") {
         width = 12;
       }
@@ -15,18 +52,34 @@ export class NewsItem extends Component {
             <div className="row" id="news-container">
             { imgURL !== "" &&
               <div className="col-lg-4">
-              {/* <div className="logo"> */}
-                <a href={this.props.url} target="_blank" >
+                <a href={this.props.url} onClick={() => this.updateCategory()} target="_blank" rel="noopener noreferrer">
                   <img className="img-fluid logo-img" src={imgURL} alt={this.props.source.id}/>
                 </a>
-              {/* </div> */}
               </div>
             }              
               <div className={"col-lg-"+width} id="news-content">
-                <a href={this.props.url} target="_blank">
-                  <h3 className="text-left news-title"> {title} </h3>
+                <a href={this.props.url} onClick={() => this.updateCategory()} target="_blank" rel="noopener noreferrer">
+                  <h2 className="news-title"> {title} </h2>
                 </a>
+                {
+                  this.props.isFull &&
+                  <h4 className="source-name">{source}</h4>
+                }
                 <p className="text-left article-desc"> {this.props.description} </p>
+                <div className="row">
+                  {
+                    this.props.category === "headline" &&
+                    (
+                      <div>
+                        <Link to={"/fullspectrum/" + this.props.article_id}>
+                          <img className="full-spectrum-icon" src={spectrum} alt="full-spectrum"/>
+                        </Link>
+                        <Link className="full-spectrum-text" to={"/fullspectrum/" + this.props.article_id}>Full Spectrum</Link>
+                      </div>
+                    )
+                    }
+                    <p className="timestamp"><Moment fromNow>{this.props.publishedAt}</Moment></p>
+                </div>
               </div>
             </div>
           </div>
---
+++ src/pages/FullCoveragePage.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import NewsItem from '../components/NewsItem.js'
+import { BeatLoader } from 'react-spinners';
+
+
+export class FullCoveragePage extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state= {
+            article:this.props.articles['headline'][this.props.match.params.id],
+            articles:[],
+            isLoaded:false,
+        }
+    }
+    componentDidMount() {
+        let title = this.state.article['title']
+        title = title.substring(0, title.lastIndexOf(" - "));
+        let url = "https://api.spectrumnews.me/v1/spectrum/" + encodeURI(title);
+        fetch(url, { 
+            method: "GET", 
+            headers: {
+                "Content-Type": "text/plain"
+            },
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+            this.setState({
+                articles:data,
+                isLoaded:true,
+            })
+        })
+        .catch((err) => {
+            console.log(err.message);
+        })
+    }
+    
+    render() {
+        let article = this.props.articles['headline'][this.props.match.params.id];
+
+        if(!this.state.isLoaded) {
+            return (
+              <div className='sweet-loading'>
+              <BeatLoader
+                sizeUnit={"px"}
+                size={20}
+                color={'#E08D4F'}
+                loading={!this.state.isLoaded}
+              />
+            </div> 
+            );
+          }
+        return (
+            <div className='full-coverage'>
+                <div className='container'>
+                    <hr className='shortBar'/>
+                    <h3 className='intro-title'>{this.state.article.title}</h3>
+                </div>
+                <div className='container'>
+                    <img className="img-fluid logo-img" src={article.urlToImage} alt=""/>               
+                </div>
+                <div className='container'>
+                <hr className='longBar'/>
+                <h3>See related articles on the topic below</h3>
+                    {
+                        this.state.articles.map((article, i) => {
+                            return (
+                                <NewsItem {...article} title={article['title']} key={i} isFull={true}/>
+                            )
+                        })
+                    }
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default FullCoveragePage;
